56dc8d47f93eebdbe1513cf3cf5bed3668734db0
kimkyury <mandarining0918@gmail.com>
2021-07-25 19:31:19 +0900

    Feat: 미들웨어 morgan사용하기

--- src/server.js
+++ src/server.js
@@ -1,8 +1,25 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
