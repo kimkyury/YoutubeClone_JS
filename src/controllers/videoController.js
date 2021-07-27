2982992a845d4d844a2d5010471cd93b609ef39f
kimkyury <mandarining0918@gmail.com>
2021-07-27 22:32:02 +0900

    Feat: Mixin을 이용한 리스트 띄우기

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,17 +2,31 @@ export const trending = (req, res) => {
   const videos = [
     {
       title: "Hamburger",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
     {
       title: "Sandwich",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
     {
       title: "Chicken",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
     },
   ];
   return res.render("home", { pageTitle: "Home", videos });
 };
-
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
